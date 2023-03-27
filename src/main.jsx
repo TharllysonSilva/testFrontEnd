2f46c1f98475978624f43ed7bb4fdcec495bedf4
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-03-27 01:00:21 -0300

    teste front end

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+//páginas
+
+import Home from "./routes/Home";
+import NewPost from "./routes/NewPost";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [{
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path: "/new",
+      element: <NewPost/>,
+    }
+  ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
