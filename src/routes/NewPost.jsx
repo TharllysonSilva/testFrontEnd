cb1a91ea34247ecc783b5712573c22cb99c6bf2c
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-03-27 02:02:23 -0300

    criação do corpo da aplicação do new post

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function NewPost() {
-  return <div>NewPost</div>;
+  return (
+    <div className="new-post">
+      <h2>Inserir novo Post: </h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Digite o título"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo</label>
+          <textarea
+            name="body"
+            id="body"
+            placeholder="Digite o título"
+          ></textarea>
+        </div>
+        <input type="submit" value="Criar Post" className="btn" />
+      </form>
+    </div>
+  );
 }
 
 export default NewPost;
