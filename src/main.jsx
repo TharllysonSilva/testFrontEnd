30f95d1a3ea02b56c6a5da66d2d93f5ba22522fa
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-03-27 20:06:06 -0300

    Importa todas as páginas

--- src/main.jsx
+++ src/main.jsx
@@ -9,30 +9,36 @@ import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import NewPost from "./routes/NewPost";
 import Comments from "./routes/Comments";
+import Users from "./routes/Users";
 
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     element: <App />,
-    children: [{
-      path: "/",
-      element: <Home/>,
-    },
-    {
-      path: "/new",
-      element: <NewPost/>,
-    },
-    {
-      path: "/posts/:id/comments",
-      element: <Comments/>,
-    },
-  ],
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <NewPost />,
+      },
+      {
+        path: "/:id/users",
+        element: <Users />,
+      },
+      {
+        path: "/posts/:id/comments",
+        element: <Comments />,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
