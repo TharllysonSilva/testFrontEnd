7a85e6529d9494b7dd1abe8f7911f0da676edbed
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-03-27 02:01:47 -0300

    utilizando a config.js para importar a url do caminho da api

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import blogFetch from "../axios/config";
 
 import { useState, useEffect } from "react";
 
@@ -11,9 +11,7 @@ const Home = () => {
 
   const getPost = async () => {
     try {
-      const response = await axios.get(
-        " https://jsonplaceholder.typicode.com/posts"
-      );
+      const response = await blogFetch.get("/posts");
 
       const data = response.data;
       setPosts(data);
