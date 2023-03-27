351e406c7fedc5658a2c5872b5260363b4245276
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-03-27 20:17:45 -0300

    Usuarios importados

--- src/routes/Users.jsx
+++ src/routes/Users.jsx
@@ -1,47 +1,44 @@
 import blogFetch from "../axios/config";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-import { useNavigate } from "react-router-dom";
-
-import "./Users.css";
+import './Users.css';
 
 const Users = () => {
-  const [users, setUsers] = useState([]);
-
-  const getUsers = async () => {
-    try {
-      const response = await blogFetch.get("/users");
-
-      const data = response.data;
-      setUsers(data);
-    } catch (error) {
-      console.log(error);
-    }
-    
-    
+    const [users, setUsers] = useState([]);
+  
+    const getUsers = async () => {
+      try {
+        const response = await blogFetch.get("/users");
+  
+        const data = response.data;
+        setUsers(data);
+      } catch (error) {
+        console.log(error);
+      }
+      
+    };
+  
+    useEffect(() => {
+      getComments();
+    }, []);
+  
+    return (
+      <div className="users">
+        <h1>Comments</h1>
+        {users.length === 0 ? (
+          <p>Carregando...</p>
+        ) : (
+          users.map((user) => (
+            <div className="users" key={user.id}>
+              <h2>{user.name}</h2>
+              <p>{user.username}</p>
+              <p1>{user.email}</p1>
+            </div>
+          ))
+        )}
+      </div>
+    );
   };
-
-  useEffect(() => {
-    getUsers();
-  }, []);
-
-  return (
-    <div className="users">
-      <h1>Users</h1>
-      {users.length === 0 ? (
-        <p>Carregando...</p>
-      ) : (
-        users.map((user) => (
-          <div className="users" key={user.id}>
-            <h2>{user.name}</h2>
-            <p>{user.username}</p>
-            <p>{user.email}</p>
-          </div>
-        ))
-      )}
-    </div>
-  );
-};
-
-export default Users;
+  
+  export default Comments;
